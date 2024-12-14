ebab9e1ea5235f8b2bbeef60796c7bee77899eb4
kayYZ1 <overek7133@gmail.com>
2024-12-14 09:14:25 +0100

    chore: Initial config

---
+++ src/__tests__/app.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'bun:test';
+import app from "../index"
+
+const mockRequest = async (url: RequestInfo | URL, options = {}) => {
+  const response = await app.fetch(new Request(url, options));
+  return response;
+};
+
+describe('Hono Application', () => {
+  it('should return Hello Hono! for GET /', async () => {
+    const response = await mockRequest('http://localhost:7676/');
+    const text = await response.text();
+
+    expect(response.status).toBe(200);
+    expect(text).toBe('Hello Hono!');
+  });
+});
