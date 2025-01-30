625d7ecc0c569ccfe06a82e364e225f2c81aaf85
kayYZ1 <overek7133@gmail.com>
2025-01-30 11:40:50 +0100

    fix: move env variables outside of createClient

--- src/db/turso.ts
+++ src/db/turso.ts
@@ -1,15 +1,19 @@
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 
+const url =
+  process.env.NODE_ENV === 'production'
+    ? (process.env.TURSO_DATABASE_URL_PROD as string)
+    : (process.env.TURSO_DATABASE_URL as string);
+
+const authToken =
+  process.env.NODE_ENV === 'production'
+    ? (process.env.TURSO_AUTH_TOKEN_PROD as string)
+    : (process.env.TURSO_AUTH_TOKEN as string);
+
 const turso = createClient({
-  url:
-    process.env.NODE_ENV === 'production'
-      ? (process.env.TURSO_DATABASE_URL_PROD as string)
-      : (process.env.TURSO_DATABASE_URL as string),
-  authToken:
-    process.env.NODE_ENV === 'production'
-      ? (process.env.TURSO_AUTH_TOKEN_PROD as string)
-      : (process.env.TURSO_AUTH_TOKEN as string),
+  url,
+  authToken,
 });
 
 export const db = drizzle(turso);
